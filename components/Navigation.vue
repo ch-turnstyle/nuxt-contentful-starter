6005a1be43ca9bb8a26eda26520732c7a0c7396f
Christine Hager <christine@turnstyle.studio>
2025-04-23 17:06:15 -0700

    Customize nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -10,31 +10,62 @@ const { data } = await useAsyncData(async () => {
   });
 });
 
-const menuItems = data.value?.items[0].fields.menuItems;
+const navItemsMap = computed(() => {
+  const menuItems = data.value?.items[0].fields.menuItems;
+  const map = menuItems.map((group: ContentfulMenuGroup) => {
+    const groupLabel = group.fields.groupName;
+    let groupIcon = 'i-lucide-credit-card';
+    groupLabel === 'Pricing' && (groupIcon = 'i-lucide-currency');
+    groupLabel === 'About Us' && (groupIcon = 'i-lucide-users-round');
+    const hasGroupLink = group.fields.groupLink;
+    const hasChildren = group.fields.featuredPages;
+    
+    return {
+      label: groupLabel,
+      icon: groupIcon,
+      to: hasGroupLink ? `/${group.fields.groupLink?.fields.slug}` : '',
+      children: hasChildren
+        ? group.fields.featuredPages?.map(page => ({
+            label: page.fields.pageName,
+            description: page.fields.internalName,
+            to: `/${page.fields.slug}`,
+          }))
+        : [],
+    }
+  });
+  return map;
+});
 
-const navItems = ref<NavigationMenuItem[]>(menuItems.map((group: ContentfulMenuGroup) => {
-  const groupLabel = group.fields.groupName;
-  let groupIcon = 'i-lucide-credit-card';
-  groupLabel === 'Pricing' && (groupIcon = 'i-lucide-currency');
-  groupLabel === 'About Us' && (groupIcon = 'i-lucide-users-round');
-  const hasGroupLink = group.fields.groupLink;
-  const hasChildren = group.fields.featuredPages;
-  
-  return {
-    label: groupLabel,
-    icon: groupIcon,
-    to: hasGroupLink ? `/${group.fields.groupLink?.fields.slug}` : '',
-    children: hasChildren
-      ? group.fields.featuredPages?.map(page => ({
-          label: page.fields.pageName,
-          description: page.fields.internalName,
-          to: `/${page.fields.slug}`,
-        }))
-      : [],
-  }
-}));
+const navItems = ref<NavigationMenuItem[]>([
+  {
+    label: 'Home',
+    icon: 'i-lucide-house',
+    to: '/',
+    slot: 'home' as const
+  },
+  ...navItemsMap.value,
+]);
 </script>
 
 <template>
-  <UNavigationMenu :items="navItems" content-orientation="vertical" class="bg-white w-full justify-center z-50" />
+  <UContainer>
+    <UNavigationMenu
+      :items="navItems"
+      content-orientation="vertical"
+      variant="link"
+      class="bg-white w-full justify-center z-50"
+      :ui="{
+        root: '[&>div]:w-full'
+      }"
+    >
+    </UNavigationMenu>
+  </UContainer>
 </template>
+
+<style>
+@reference '@/assets/css/main.css';
+
+[data-menu-item]:first-child {
+  @apply mr-auto;
+}
+</style>
